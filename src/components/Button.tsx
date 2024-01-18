1674c1821856edb9b49860897bdb8447884e870c
Belal Khalifa <belalkhalifa@Belals-MacBook-Pro.local>
2024-01-18 16:45:22 +0100

    Update Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { HTMLAttributes } from "react";
 import { FC } from "react";
+import { cn } from "../lib/utils";
 
-interface ButtonProps {}
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {}
 
-const Button: FC<ButtonProps> = ({}) => {
+const Button: FC<ButtonProps> = ({ className, children, ...props }) => {
   return (
-    <button style={{backgroundColor: "gray", padding: "10px", borderRadius: "18px", border: "1.25px solid blue"}} className="bg-blue-400 rounded-md border-2 border-white flex items-center justify-center h-10">
-      This is a CC Button-Component
+    <button
+      style={{
+        backgroundColor: "gray",
+        padding: "10px",
+        borderRadius: "18px",
+        border: "1.25px solid blue",
+      }}
+      className={cn(className)}
+      {...props}
+    >
+      {children}
     </button>
   );
 };
