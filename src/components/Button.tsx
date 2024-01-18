5170d2fa3513bfb72b6ecabfde18dd8667b8637a
Belal Khalifa <belalkhalifa@Belals-MacBook-Pro.local>
2024-01-18 16:31:22 +0100

    Test

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {}
 
 const Button: FC<ButtonProps> = ({}) => {
   return (
-    <button className="bg-blue-400 rounded-md border-2 border-white flex items-center justify-center h-10">
+    <button style={{backgroundColor: "gray", padding: "10px", borderRadius: "18px", border: "1.25px solid blue"}} className="bg-blue-400 rounded-md border-2 border-white flex items-center justify-center h-10">
       This is a CC Button-Component
     </button>
   );
